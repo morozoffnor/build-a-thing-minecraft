75db988431b70199ff301a7c183fd9843e43f707
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2022-07-16 15:49:35 +0300

    initial

---
+++ src/main/java/buildathing/buildathing/Buildathing.java
@@ -0,0 +1,17 @@
+package buildathing.buildathing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Buildathing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
