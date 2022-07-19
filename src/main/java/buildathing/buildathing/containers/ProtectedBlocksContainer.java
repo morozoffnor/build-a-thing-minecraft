03549bcd6486e2e3a846cdafa7742390ce957b33
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2022-07-19 14:48:49 +0300

    comments

--- src/main/java/buildathing/buildathing/containers/ProtectedBlocksContainer.java
+++ src/main/java/buildathing/buildathing/containers/ProtectedBlocksContainer.java
@@ -4,16 +4,21 @@ import org.bukkit.Location;
 
 import java.util.List;
 
+/*
+This is a singleton class containing everything about protected blocks
+ */
 public class ProtectedBlocksContainer {
 
     public List<Location> protectedBlocks;
 
     private static ProtectedBlocksContainer instance = null;
-    private ProtectedBlocksContainer(){
+
+    private ProtectedBlocksContainer() {
 
     }
-    public static ProtectedBlocksContainer getInstance(){
-        if(instance==null){
+
+    public static ProtectedBlocksContainer getInstance() {
+        if (instance == null) {
             instance = new ProtectedBlocksContainer();
         }
         return instance;
