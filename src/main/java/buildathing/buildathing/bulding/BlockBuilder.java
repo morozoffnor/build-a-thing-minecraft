cc141211e154511a0ef3334a487f77dfd6b21173
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2022-07-22 11:38:45 +0300

    createPlatform function

--- src/main/java/buildathing/buildathing/bulding/BlockBuilder.java
+++ src/main/java/buildathing/buildathing/bulding/BlockBuilder.java
@@ -11,9 +11,9 @@ public class BlockBuilder {
         // pass the game center to GameManager
         GameManager.getInstance().gameCenter = location;
         // create a platform
-        for (int x = (int)location.getX()-radius; x < (int)location.getX()+radius; x++) {
-            for (int z = (int)location.getZ()-radius; z < (int)location.getZ()+radius; z++) {
-                Block block = location.getWorld().getBlockAt(x, (int)location.getY(), z);
+        for (int x = (int) location.getX() - radius; x < (int) location.getX() + radius; x++) {
+            for (int z = (int) location.getZ() - radius; z < (int) location.getZ() + radius; z++) {
+                Block block = location.getWorld().getBlockAt(x, (int) location.getY(), z);
                 block.setType(Material.WHITE_CONCRETE);
                 ProtectedBlocksContainer.getInstance().protectedBlocks.add(block.getLocation());
             }
